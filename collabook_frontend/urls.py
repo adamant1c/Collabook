094c1567c4d671a3c508d1693f14f740020779dd
alessandro <palexander82@gmail.com>
2026-01-24 20:28:57 +0100

    Sitemap updated

--- collabook_frontend/urls.py
+++ collabook_frontend/urls.py
@@ -35,24 +35,26 @@ def robots_txt(request):
 
 def sitemap_xml(request):
     xml_content = """<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>https://collabook.click/</loc>
-    <priority>1.0</priority>
-  </url>
-  <url>
-    <loc>https://collabook.click/about/</loc>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>https://collabook.click/game/rules/</loc>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>https://collabook.click/accounts/register/</loc>
-    <priority>0.7</priority>
-  </url>
-</urlset>"""
+                        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+                          <url>
+                            <loc>https://collabook.click/</loc>
+                            <lastmod>2026-01-24</lastmod>
+                            <changefreq>weekly</changefreq>
+                            <priority>1.0</priority>
+                          </url>
+                          <url>
+                            <loc>https://collabook.click/about/</loc>
+                            <lastmod>2026-01-24</lastmod>
+                            <changefreq>monthly</changefreq>
+                            <priority>0.8</priority>
+                          </url>
+                          <url>
+                            <loc>https://collabook.click/game/rules/</loc>
+                            <lastmod>2026-01-24</lastmod>
+                            <changefreq>monthly</changefreq>
+                            <priority>0.8</priority>
+                          </url>
+                """
     return HttpResponse(xml_content, content_type="application/xml")
 
 urlpatterns = [
